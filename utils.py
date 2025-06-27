26d5dde18091ce780e304b08df663b524ec9130a
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-06-27 17:56:30 +0300

    utils functions

--- utils.py
+++ utils.py
@@ -1,3 +1,13 @@
 def format_decimal_to_percent(win_decimal):
     win_percent_str = f"{win_decimal * 100:.1f}%"
     return win_percent_str
+
+
+def calculate_custom_score(df):
+    return (
+        (df["Win %"] * 400)
+        + (df["Games Played"] * 2)
+        + (df["Championships"] * 50)
+        + (df["Playoff Appearances"] * 25)
+        + df["Initial_Score"]
+    )
