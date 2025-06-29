3fe3b067b48ea80d1d847627516433c31ef99b8f
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-06-29 09:36:15 +0300

    add util fns for percentage - decimal conversion

--- utils.py
+++ utils.py
@@ -1,14 +1,34 @@
-def format_decimal_to_percent(win_decimal):
-    win_percent_str = f"{win_decimal * 100:.1f}%"
-    return win_percent_str
+import pandas as pd
+import numpy as np
+
+
+def format_decimal_to_percent_columns(df):
+    percent_fields = ["Win %", "Playoff Rate"]
+    for col in percent_fields:
+        df[col] = df[col].apply(lambda x: f"{x * 100:.1f}%" if pd.notnull(x) else x)
+    return df
 
 
 def calculate_custom_score(df):
     return (
         (df["Win %"] * 400)
-        + (df["Games Played"] * 2)
-        + (df["Championships"] * 50)
         + (df["Playoff Appearances"] * 25)
         + (df["Chevrons/game"] * 30)
         + (df["K/D ratio"] * 100)
     )
+
+
+def compute_percentiles(df, features):
+    percentiles = {}
+    for feature in features:
+        percentiles[feature] = df[feature].rank(pct=True, ascending=True)
+    return percentiles
+
+
+def percent_str_to_float(s):
+    if isinstance(s, str) and s.endswith("%"):
+        return float(s.strip("%")) / 100
+    elif pd.isnull(s):
+        return np.nan
+    else:
+        return float(s)
