496827d8e754dd88a3140fe5f4caae2ce9da53d0
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-06-29 09:40:10 +0300

    add simple navigation

--- main.py
+++ main.py
@@ -1,37 +1,11 @@
 import streamlit as st
-import pandas as pd
-import plotly.express as px
-from utils import format_decimal_to_percent
+from training_result import show_training_result
+from player_comparison import show_player_comparison
 
-df = pd.read_json("player_data.json")
+st.sidebar.title("Navigation")
+page = st.sidebar.selectbox("Choose a page", ["Training Results", "Player Comparison"])
 
-df["Win %"] = df["Win %"].apply(format_decimal_to_percent)
-
-st.title("Player Scores and Tiers Dashboard")
-
-tier_filter = st.multiselect(
-    "Select Tiers to display",
-    options=df["Tier"].unique(),
-    default=df["Tier"].unique(),
-)
-
-filtered_df = df[df["Tier"].isin(tier_filter)]
-
-score_type = "Predicted_Score"
-
-fig = px.scatter(
-    filtered_df,
-    x=score_type,
-    y="K/D ratio",
-    color="Tier",
-    hover_data=["Player", "Win %", "Chevrons/game", "Playoff Appearances"],
-    title=f"{score_type} vs K/D Ratio by Player Tier",
-)
-
-st.plotly_chart(fig)
-
-st.dataframe(
-    filtered_df[
-        ["Player", "Tier", "Win %", "K/D ratio", "Chevrons/game", score_type]
-    ].sort_values(by=score_type, ascending=False)
-)
+if page == "Training Results":
+    show_training_result()
+elif page == "Player Comparison":
+    show_player_comparison()
