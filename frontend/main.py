42377bcb2f012c819dae5c9fa6850794d7e0053a
Borislav Tsvetkov <borislav.tsvetkov02@gmail.com>
2025-07-07 19:25:21 +0300

    add new page to navigation

--- frontend/main.py
+++ frontend/main.py
@@ -1,20 +1,26 @@
 import streamlit as st
 from display_player_prediction import display_player_prediction
 from player_comparison import show_player_comparison
-from tier_list import show_team_tier_list
 from display_team_prediction import show_team_analysis
+from team_comparison import show_team_comparison
 
 st.sidebar.title("Navigation")
 page = st.sidebar.selectbox(
     "Choose a page",
-    ["Player Prediction", "Player Comparison", "Team Tier List", "Team Prediction"],
+    [
+        "Player Prediction",
+        "Player Comparison",
+        "Team Tier List",
+        "Team Prediction",
+        "Team Comparison",
+    ],
 )
 
 if page == "Player Prediction":
     display_player_prediction()
 elif page == "Player Comparison":
     show_player_comparison()
-elif page == "Team Tier List":
-    show_team_tier_list()
 elif page == "Team Prediction":
     show_team_analysis()
+elif page == "Team Comparison":
+    show_team_comparison()
